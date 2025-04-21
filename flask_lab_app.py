a0f5235c2be794abdf90c9f2d5e17f6cc13d0229
asa-grumdahl <grumdahla@carleton.edu>
2025-04-21 12:00:38 -0500

    added dataset.csv and updated flask_lab_app.py to be able to use it

--- flask_lab_app.py
+++ flask_lab_app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
+
 def homepage():
+    load_data()
     return "fermented greenland shark"
+
+data = []
+def load_data():
+    with open('dataset.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row)
+
+@app.route('/<row>/<column>', strict_slashes=False)
+def get_cell(row,column):
+    return data[int(row)][int(column)]
 
 if __name__ == "__main__":
     app.run()
