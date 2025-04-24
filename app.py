cb03ebe50d45507068c4fcc691c262b7e3dc185d
asa-grumdahl <grumdahla@carleton.edu>
2025-04-24 17:36:32 -0500

    implemented a very basic app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-'''Replace me with your flask app'''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def this():
+    return "fermented greenland shark"
+
+if __name__ == "__main__":
+    app.run()
