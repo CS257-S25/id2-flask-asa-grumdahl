b88e8874db7925e90b3e8ff8043f208e4c34899a
asa-grumdahl <grumdahla@carleton.edu>
2025-04-24 17:41:22 -0500

    cleaned up from the flask lab

---
+++ Tests/tests.py
@@ -0,0 +1,5 @@
+from app import *
+import unittest
+
+class TestStuff(unittest.testCase)
+    
--- app.py
+++ app.py
@@ -6,5 +6,24 @@ app = Flask(__name__)
 def this():
     return "fermented greenland shark"
 
+data = []
+def load_data():
+    with open('dataset.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row)
+
+@app.route('/<row>/<column>', strict_slashes=False)
+def get_cell(row,column):
+    return data[int(row)][int(column)]
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Nothing leads here! I recommend navigating back to the homepage."
+
+@app.errorhandler(500)
+def python_bug(e):
+    return "Some kind of bug happened..."
+
 if __name__ == "__main__":
     app.run()
