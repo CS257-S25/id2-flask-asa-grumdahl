e85c393f8e5d8059143764d486a137e4d7bb29bd
asa-grumdahl <grumdahla@carleton.edu>
2025-04-24 17:43:06 -0500

    Delete tests.py

--- tests.py
+++
@@ -1,8 +0,0 @@
-from app import *
-import unittest
-
-class TestStuff(unittest.testCase)
-    def test_route(self):
-        self.app = app.test_client()
-        response = self.app.get('/', follow_redirects = True)
-        self.assertEqual(b'fermented greenland shark', response.data)
