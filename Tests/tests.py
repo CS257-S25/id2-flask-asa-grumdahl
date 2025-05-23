74ad756f81d9ad82c334b71dca0c2d1b1bc0b44a
asa-grumdahl <grumdahla@carleton.edu>
2025-05-23 12:52:42 -0500

    last update for now

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,12 +1,16 @@
 import unittest
-from ProductionCode import data as Data
-from ProductionCode.filter import Filter
+from ProductionCode import data as datapy
+from ProductionCode.filter import Filter as filterpy
 
-class TestStuff(unittest.TestCase):
-    """ Testing area to ensure functions work before developing them. Only sufficiently relevant tests will be kept. """
+class TestSuite(unittest.TestCase):
+    """ Testing area to ensure functions work before developing them. """
+
+    def setUp(self):
+        """ Sets up the test suite. """
+        pass
 
     def test_turn_dataset_name_in_url_to_filepath(self):
-        self.assertEqual("./Dummy_data/dummy_amazon.csv",app.turn_dataset_name_in_url_to_filepath("amazon"))
+        self.assertEqual("./Dummy_data/dummy_amazon.csv",filterpy.turn_dataset_name_in_url_to_filepath("amazon"))
 
 if __name__ == "__main__":
     unittest.main()
